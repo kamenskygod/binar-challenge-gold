10d6c1df1cfb8cd84beaec6cc9b6bbeaf1201670
kamenskygod <aditya.dharmawan1995@gmail.com>
2023-04-17 09:00:11 +0700

    tambahan login pages

---
+++ src/pages/login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col, Input, Row } from 'reactstrap';
+
+const Login = () => {
+  return (
+    <div>
+      <Row>
+        <Col>
+          <Input />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Login;
